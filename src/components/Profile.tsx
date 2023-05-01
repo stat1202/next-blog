f7801e4fa37a8670b2b8efb87690366882a9f544
stat1202 <hseungjae1202@naver.com>
2023-05-01 18:11:41 +0900

    page 생성

---
+++ src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import profileImg from "../../public/images/profile.jpg"
+import Link from 'next/link'
+
+export default function Profile() {
+  return (
+    <div className='flex flex-col items-center pt-4'>
+      <Image  src={profileImg} alt="profile" 
+      className='w-80 h-80 max-sm:w-60 max-sm:h-60 rounded-full'></Image>
+      <h2 className='pt-2.5 font-bold text-2xl max-sm:text-lg'>Hi, I'm SeungJae Han</h2>
+      <p className='pt-2.5 font-semibold max-sm:text-base'>Frontend engineer</p>
+      <Link href="/contact"
+      className='mt-2.5 px-2 py-1 bg-stone-900 rounded font-semibold text-white'>Contact Me</Link>
+    </div>
+  )
+}
